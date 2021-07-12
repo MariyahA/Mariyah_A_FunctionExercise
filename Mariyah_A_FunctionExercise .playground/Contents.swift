b60d516f4e4593f2abea32ba356fdb268d0bb513
MariyahA <86676361+MariyahA@users.noreply.github.com>
2021-07-12 11:14:00 -0500

    Add files via upload

---
+++ Mariyah_A_FunctionExercise .playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+/*Function creates a place holder for the numbers of choice
+and returns the sum of the numbers added together
+*/
+func add(num1: Int, num2: Int) -> Int{
+    return num1 + num2
+}
+//Prints the sum of the numbers
+print(add(num1: 40, num2: 50))
